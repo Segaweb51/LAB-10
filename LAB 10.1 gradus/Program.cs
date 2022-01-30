cf5fce19a1076b1fef7fd087afe32523838c7911
Sergey <Sergey@DESKTOP-PQ9HUE6>
2022-01-30 22:30:32 +0300

    Добавьте файлы проекта.

---
+++ LAB 10.1 gradus/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_10._1_gradus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Angular angular = new Angular();
+            angular.setValues(50, 0, 0, 0);
+            angular.printValues();
+            Console.ReadKey();
+        }
+
+        class Angular
+        {
+
+            public double gradus;
+            public double min;
+            public double sec;
+            private double Radians;
+            public void setValues(double _gradus, double _min, double _sec, double _Radians)
+            {
+                gradus = _gradus;
+                min = _min;
+                sec = _sec;
+                Radians = _Radians = (gradus + min / 60 + sec / 3600) * (Math.PI) / 180;
+
+                if (gradus < 0)
+                    Radians = -Radians;
+
+            }
+
+            public void printValues()
+            {
+                System.Console.Write(gradus + "-gradus." + min + "-min." + sec + "-sec." + Radians + "-Radians");
+                System.Console.WriteLine();
+            }
+        }    
+    }
+}
---
+++ LAB 10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+      
+        }
+    }
+    class Cat
+    {
+        string name;
+        string colore;
+        int age;
+
+        Cat(string n, string c, int a)
+        {
+            name = n;
+            colore = c;
+            age = a;
+        }
+        void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+}
